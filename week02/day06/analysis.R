8509b93224cc1ccba272991aa2b17821a743cbc0
James <quangthaivu95@gmail.com>
2026-05-11 13:46:06 +0900

    feat: day06 - add folder for day 06 including R analysis script, Rmd report, CSS style, and output folder

---
+++ week02/day06/analysis.R
@@ -0,0 +1,8 @@
+#============================================================
+# Task 005 — Linear Regression
+# Outcome: Systolic blood pressure (BPSysAve)
+# Predictors: Age, BMI, Gender
+# Dataset: NHANES adults (Age >= 18)
+# Author: [T.V.Q]
+# Date: [11 May 2026]
+# ============================================================
